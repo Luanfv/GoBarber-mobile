1c2167d67c8a9179be60cd85f7542e4113d42e5e
Luan <luanfrancavieira@gmail.com>
2020-08-12 19:01:52 -0300

    routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { AuthRoutes } from './routes';
 
 const App: React.FC = () => {
     return (
-        <View>
-            <Text>GoBarber</Text>
-        </View>
+        <NavigationContainer>
+            <StatusBar barStyle="light-content" backgroundColor="#321e38" />
+            <View style={{ 
+                backgroundColor: '#321e38', 
+                flex: 1
+            }}>
+                <AuthRoutes />
+            </View>
+        </NavigationContainer>
     );
 }
 
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './style';
+
+const SignUp: React.FC = () => {
+    return (
+        <Container>
+            
+        </Container>
+    );
+}
+
+export default SignUp;
