e571d1758f030b05dd94763d4d988cadd3799754
Luan <luanfrancavieira@gmail.com>
2020-08-16 15:25:06 -0300

    fix: uscecallback

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -64,7 +64,7 @@ const SignUp: React.FC = () => {
                 'Ocorreu um erro ao fazer o cadastro, tente novamente.',
             );
         }
-    }, []);
+    }, [navigation]);
 
 
     return (
