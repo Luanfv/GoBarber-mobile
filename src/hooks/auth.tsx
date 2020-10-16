bec0aa992409e48b40f850c32096d2290ff80a2b
Luan França Vieira <luanfrancavieira@gmail.com>
2020-10-15 22:07:28 -0300

    list providers

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -46,8 +46,11 @@ export const AuthProvider: React.FC = ({ children }) => {
                 '@GoBarber:user',
             ]);
 
-            if (token[1] && user[1])
+            if (token[1] && user[1]) {
+                api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
                 setData({ token: token[1], user: JSON.parse(user[1]) });
+            }
                 
             setIsLoading(false);
         }
@@ -67,6 +70,8 @@ export const AuthProvider: React.FC = ({ children }) => {
             ['@GoBarber:token', token],
             ['@GoBarber:user', JSON.stringify(user)],
         ]);
+
+        api.defaults.headers.authorization = `Bearer ${token}`;
 
         setData({ token , user});
     }, []);
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,58 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import { useAuth } from './../../hooks/auth';
 
-import { Container, Header, HeaderTitle, UserName, ProfileButton, UserAvatar } from './style';
+import api from '../../services/api';
+
+import { 
+    Container, 
+    Header, 
+    HeaderTitle, 
+    UserName, 
+    ProfileButton, 
+    UserAvatar,
+    ProvidersList,
+    ProviderContainer,
+    ProviderAvatar,
+    ProviderInfo,
+    ProviderName,
+    ProviderMeta,
+    ProviderMetaText,
+    ProvidersListTitle
+} from './style';
+
+export interface Provider {
+    id: string;
+    name: string;
+    avatar_url: string;
+}
 
 const Dashboard: React.FC = () => {
     const { signOut, user } = useAuth();
     const { navigate } = useNavigation();
 
-    const navigateToProfile = useCallback(() => navigate('Profile'), [navigate]);
+    const [providers, setProviders] = useState<Provider[]>([]);
+
+    useEffect(() => {
+        api.get('/providers')
+        .then(response => {
+            setProviders(response.data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }, []);
+
+    const navigateToProfile = useCallback(() => {
+        // navigate('Profile');
+        signOut();
+    }, [navigate, signOut]);
+
+    const navigateToCreateAppointment = useCallback((providerId) => {
+        navigate('CreateAppointment', { providerId });
+    }, [navigate]);
 
     return (
         <Container>
@@ -24,6 +67,42 @@ const Dashboard: React.FC = () => {
                     <UserAvatar source={{ uri: user.avatar_url }} />
                 </ProfileButton>
             </Header>
+
+            <>
+                <ProvidersList 
+                    data={providers}
+                    keyExtractor={provider => provider.id}
+                    ListHeaderComponent={
+                        <ProvidersListTitle>
+                            Cabeleleireiros
+                        </ProvidersListTitle>
+                    }
+                    renderItem={({ item: provider }) => (
+                        <ProviderContainer onPress={() => navigateToCreateAppointment(provider.id)}>
+                            <ProviderAvatar source={{ uri: provider.avatar_url }} />
+
+                            <ProviderInfo>
+                                <ProviderName>
+                                    { provider.name }
+                                </ProviderName>
+
+                                <ProviderMeta>
+                                    <Icon name="calendar" size={14} color="#ff9000" />
+                                    <ProviderMetaText>
+                                        Segunda à sexta
+                                    </ProviderMetaText>
+                                </ProviderMeta>
+                                <ProviderMeta>
+                                    <Icon name="clock" size={14} color="#ff9000" />
+                                    <ProviderMetaText>
+                                        8hrs às 18hrs
+                                    </ProviderMetaText>
+                                </ProviderMeta>
+                            </ProviderInfo>
+                        </ProviderContainer>
+                    )}
+                />
+            </>
         </Container>
     );
 }
