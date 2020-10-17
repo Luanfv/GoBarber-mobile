dc6162f1c280aeb3874e9f18be46a37625c07586
Luan França Vieira <luanfrancavieira@gmail.com>
2020-10-17 17:10:52 -0300

    header of create appointment

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useAuth } from '../../hooks/auth';
+
+import { Container, Header, BackButton, HeaderTitle, UserAvatar, } from './style';
+
+interface RouteParams {
+    providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
+    const route = useRoute();
+    const { user } = useAuth();
+    const { goBack } = useNavigation();
+
+    const { providerId } = route.params as RouteParams;
+
+    const navigateBack = useCallback(() => {
+        goBack();
+    }, []);
 
     return (
-        <View style={{ flex: 1, justifyContent: 'center' }}></View>
+        <Container>
+            <Header>
+                <BackButton onPress={navigateBack}>
+                    <Icon name="chevron-left" size={24} color="#999591" />
+                </BackButton>
+
+                <HeaderTitle>
+                    Cabeleireiros
+                </HeaderTitle>
+
+                <UserAvatar source={{ uri: user.avatar_url }} />
+            </Header>
+        </Container>
     );
 }
 
